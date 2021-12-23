e38df7bef8783e2e85577ec444c9a8a0b2f1f543
zhouqizhu <1427969767@qq.com>
2021-12-23 14:24:51 +0800

    完成了首页组件的拆分

---
+++ src/views/home/Docker.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="docker">
+    <div v-for="(item, index) in dockerList" :key="item.icon"
+      :class="{'docker__item': true, 'docker__item--active': index === 0}"
+    >
+      <div class="iconfont" v-html="item.icon" />
+      <div class="docker__title">{{item.text}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Docker',
+  setup () {
+    const dockerList = [
+      { icon: '&#xe6b8;', text: '首页' },
+      { icon: '&#xe746;', text: '购物车' },
+      { icon: '&#xe610;', text: '订单' },
+      { icon: '&#xe78b;', text: '我的' }
+    ]
+    return { dockerList }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/viriables.scss';
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  padding: 0 .18rem;
+  width: 100%;
+  height: .49rem;
+  border-top: .01rem solid #f1f1f1;
+  color: $content-fontcolor;
+  &__item {
+  flex: 1;
+  text-align: center;
+  .iconfont {
+    font-size: .18rem;
+    margin: .07rem 0 .02rem 0;
+  }
+  &--active {
+    color: #1fa4fc;
+  }
+  }
+  &__title {
+    font-size: .2rem;
+    transform: scale(.5, .5);
+    transform-origin: center top;
+  }
+}
+</style>
---
+++ src/views/home/StaticPart.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="position">
+    <span class="iconfont position__icon">&#xe8c0;</span>
+      东华理工大学4号教学楼322
+    <span class="iconfont position__notice">&#xe8ae;</span>
+  </div>
+  <div class="search">
+    <span class="iconfont">&#xe6ac;</span>
+    <span class="search__text"></span>
+  </div>
+  <div class="banner">
+    <img class="banner__img"
+      src="http://www.dell-lee.com/imgs/vue3/banner.jpg" alt=""
+    >
+  </div>
+  <div class="icons">
+      <div class="icons__item" v-for="item in iconList" :key="item.id">
+        <img class="icons__item__img" :src="`http://www.dell-lee.com/imgs/vue3/${item.imgName}.png`">
+        <p class="icons__item__desc">{{item.desc}}</p>
+      </div>
+    </div>
+    <div class="gap"></div>
+</template>
+
+<script>
+export default {
+  name: 'StaticPart',
+  setup () {
+    const iconList = [
+      { imgName: '超市', desc: '超市便利' },
+      { imgName: '菜市场', desc: '菜市场' },
+      { imgName: '水果店', desc: '水果店' },
+      { imgName: '鲜花', desc: '鲜花绿植' },
+      { imgName: '医药健康', desc: '医药健康' },
+      { imgName: '家居', desc: '家居时尚' },
+      { imgName: '蛋糕', desc: '烘培蛋糕' },
+      { imgName: '签到', desc: '签到' },
+      { imgName: '大牌免运', desc: '大牌免运' },
+      { imgName: '红包', desc: '红包套餐' }
+    ]
+    return { iconList }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/viriables.scss';
+@import '../../style/mixins.scss';
+.position {
+  position: relative;
+  padding: .16rem .24rem .16rem 0;
+  line-height: .22rem;
+  font-size: .16rem;
+  color: $content-fontcolor;
+  @include ellipsis;
+  .position__icon {
+    position: relative;
+    top: .01rem;
+    margin-right: .08rem;
+    font-size: .2rem;
+  }
+  .position__notice {
+    position: absolute;
+    right: 0;
+    top: .17rem;
+    font-size: .2rem;
+  }
+}
+.search {
+  margin-bottom: .12rem;
+  line-height: .32rem;
+  background: #f5f5f5;
+  color: #b7b7b7;
+  border-radius: .16rem;
+  &__icon {
+    display: inline-block;
+    padding: 0 .08rem 0 .16rem;
+  }
+  &__text {
+    display: inline-block;
+    font-size: .14rem;
+  }
+}
+.banner {
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__img {
+    width: 100%;
+  }
+}
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: .16rem;
+  &__item {
+    width: 20%;
+    &__img {
+      display: block;
+      width: .4rem;
+      height: .4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      text-align: center;
+      margin: .06rem 0 .16rem 0;
+      color: $content-fontcolor;
+    }
+  }
+}
+.gap {
+  margin: 0 -.18rem;
+  height: .1rem;
+  background: $content-bgColor;
+}
+</style>
