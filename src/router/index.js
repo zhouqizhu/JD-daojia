704fc0307d50b0d7d87dc4dd99293bd38b9daf7c
zhouqizhu <1427969767@qq.com>
2021-12-23 20:24:49 +0800

    完成了注册和登陆页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,32 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/home/Home'
+import Login from '../views/login/Login'
+import Register from '../views/register/Register'
 
 const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    beforeEnter (to, from, next) {
+      const isLogin = localStorage.isLogin
+      if (isLogin) next({ name: 'Home' })
+      else next()
+    }
+  }
   // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
+  //   path: '/login',
+  //   name: 'Login',
   //   // route level code-splitting
   //   // this generates a separate chunk (about.[hash].js) for this route
   //   // which is lazy-loaded when the route is visited.
@@ -19,6 +37,13 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const { isLogin } = localStorage
+  const { name } = to
+  const isLoginOrRegister = (name === 'Login' || name === 'Register');
+  (isLogin || isLoginOrRegister) ? next() : next({ name: 'Login' })
 })
 
 export default router
